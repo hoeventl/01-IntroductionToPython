148b25d269081d4c446a7c2fd938d67d18663497
hoeventl <hoeventl@rose-hulman.edu>
2018-11-26 14:32:50 -0500

    almost done

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -51,7 +51,7 @@ print(math.sqrt(41*88+4*math.cos(2)))
 print(math.sqrt(2))
 print(2**.5)
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number").
@@ -92,15 +92,20 @@ print(2**.5)
 ###############################################################################
 print(type(3.14))
 print(type("hello"))
-
-
-
-
-
-
+print(type('hello'))
+print(type('a b c'))
+print(type(3+3))
+print(type("3"+"3"))
+print(type(2**100))
+print(type(2.0**100))
+print(type(math.sin(8)))
+print(type(math.sin))
+print(type(print))
+print(type(math))
+print(type('math'))
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
